f68e0936bd4c228bc4ce3fefb96c16a4cf1bdacf
Jordan <jayala1993@outlook.com>
2016-04-20 11:54:45 -0400

    Annotated mappers without xml

--- src/main/java/py/una/pol/web/tarea4/mapper/ItemMapper.java
+++ src/main/java/py/una/pol/web/tarea4/mapper/ItemMapper.java
@@ -1,5 +1,6 @@
 package py.una.pol.web.tarea4.mapper;
 
+import org.apache.ibatis.annotations.Select;
 import py.una.pol.web.tarea4.model.Item;
 
 import java.util.List;
@@ -8,6 +9,10 @@ import java.util.List;
  * Created by jordan on 4/20/16.
  */
 public interface ItemMapper {
+
+  @Select("SELECT * FROM Item WHERE id = #{id}")
   Item getItem(int id);
+
+  @Select("SELECT * FROM Item")
   List<Item> getItems();
 }
